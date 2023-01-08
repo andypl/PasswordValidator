2047898b1bb79e45fc3de1585357b868b81e8777
anc <andrzej.czerniak@gmail.com>
2023-01-08 22:22:28 +0100

    Add some basic logging to PasswordValidator.

--- src/main/java/passwordValidator/PasswordValidator.java
+++ src/main/java/passwordValidator/PasswordValidator.java
@@ -1,5 +1,6 @@
 package passwordValidator;
 
+import lombok.extern.slf4j.Slf4j;
 import passwordValidator.dto.ValidationPasswordResult;
 
 import java.util.ArrayList;
@@ -8,6 +9,7 @@ import java.util.List;
 import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
 
+@Slf4j
 public class PasswordValidator {
     private String password;
     private List<PasswordRule> passwordRuleList;
@@ -24,10 +26,12 @@ public class PasswordValidator {
             return new ValidationPasswordResult(TRUE);
         }
 
+        log.info("Password validation failed for password: " + password + " and rules: " + rulesNotValidated);
         return new ValidationPasswordResult(FALSE, rulesNotValidated);
     }
 
     private void validatePasswordAgainstRules() {
+        log.info("Starting validation for password: " + password + " and rules: " + passwordRuleList);
         passwordRuleList.stream()
                 .filter(rule -> rule.validate(password).equals(FALSE))
                 .forEach(rule -> rulesNotValidated.add(rule.getName()));
